6e0f02afd6e9ec955cda97b50be6ea9bb3b590a7
shalu <shalushaji999@gmail.com>
2021-09-17 15:50:29 +0530

    FizzBuzz changes committed

--- kata_training/src/main/java/kata_training/FizzBuzz.java
+++ kata_training/src/main/java/kata_training/FizzBuzz.java
@@ -3,13 +3,12 @@ package kata_training;
 public class FizzBuzz {
 
 	public String play(int i) {
-		if(i%3 == 0 ) {
+		if (i % 3 == 0) {
 			return "Fizz";
 		}
-		if(i%5 ==0 ) {
+		if (i % 5 == 0) {
 			return "Buzz";
 		}
-		
 		return String.valueOf(i);
 	}
 
--- kata_training/src/test/java/kata_training/FizzBuzzTest.java
+++ kata_training/src/test/java/kata_training/FizzBuzzTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 public class FizzBuzzTest {
 	static FizzBuzz fizzBuzz = null;
-	
+
 	@BeforeAll
 	public static void init() {
 		fizzBuzz = new FizzBuzz();
@@ -18,32 +18,35 @@ public class FizzBuzzTest {
 		String response = fizzBuzz.play(3);
 		assertEquals("Fizz", response);
 	}
+
 	@Test
 	public void testFizzNot3() {
 		String response = fizzBuzz.play(6);
 		assertEquals("Fizz", response);
 	}
+
 	@Test
 	public void testFizzBuzzZero() {
 		String response = fizzBuzz.play(0);
 		assertEquals("Fizz", response);
 	}
+
 	@Test
 	public void testBuzz() {
 		String response = fizzBuzz.play(5);
 		assertEquals("Buzz", response);
 	}
+
 	@Test
 	public void testBuzz10() {
 		String response = fizzBuzz.play(10);
 		assertEquals("Buzz", response);
 	}
+
 	@Test
 	public void testBuzz44() {
 		String response = fizzBuzz.play(44);
 		assertEquals("44", response);
 	}
 
-
-	
 }
